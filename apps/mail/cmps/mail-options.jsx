c49b64ce61dbb83fd98f652e0814984e276af2c0
TalBenAtiya <benatia.tal@gmail.com>
2022-08-25 19:20:54 +0200

    working filter and deleting mail

--- apps/mail/cmps/mail-options.jsx
+++ apps/mail/cmps/mail-options.jsx
@@ -1,22 +1,27 @@
-export function MailOptions({onFilterBy}) {
+export function MailOptions({onSetFilter}) {
 
+    
     return <section className="user-options">
 
-        <div onClick={() => onFilterBy('inbox')} className="flex" tabIndex="1">
+        <div onClick={() => onSetFilter('inbox')} className="flex" tabIndex="1">
             <img src="assets/img/inbox.png" />
             <h4>Inbox</h4>
         </div>
-        <div onClick={() => onFilterBy('starred')} className="flex" tabIndex="1">
+        <div onClick={() => onSetFilter('starred')} className="flex" tabIndex="1">
             <img src="assets/img/star2.png" />
             <h4>Starred</h4>
         </div>
-        <div onClick={() => onFilterBy('important')} className="flex" tabIndex="1">
+        <div onClick={() => onSetFilter('important')} className="flex" tabIndex="1">
             <img src="assets/img/right-arrow2.png" />
             <h4>Important</h4>
         </div>
-        <div onClick={() => onFilterBy('sent')} className="flex" tabIndex="1">
+        <div onClick={() => onSetFilter('sent')} className="flex" tabIndex="1">
             <img src="assets\img\sent.png" />
             <h4>Sent</h4>
+        </div>
+        <div onClick={() => onSetFilter('trash')} className="flex" tabIndex="1">
+            <img src="assets\img\trash.png" />
+            <h4>Trash</h4>
         </div>
     </section>
 }
