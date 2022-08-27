949195a467a66ac6c6d0ebe2bd6189ecb3a3d699
TalBenAtiya <benatia.tal@gmail.com>
2022-08-27 18:15:14 +0200

    merge

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -5,36 +5,36 @@ export class AppHeader extends React.Component {
 
     inputRef = React.createRef()
 
-    toggleMenu=()=> {
+    toggleMenu = () => {
         this.inputRef.current.classList.toggle('menu-opened')
     }
 
-    closeMenu =() => {
+    closeMenu = () => {
         this.inputRef.current.classList.remove('menu-opened')
     }
 
-    render(){
-    return <header className="app-header">
-        <div className="main-layout">
+    render() {
+        return <header className="app-header">
+            <div className="main-layout">
                 <Link to="/">
-                <div>
+                    <div>
                         <h3>Appsus</h3>
-                    <img src="assets/img/appsus-logo.png" />
-                </div>
+                        <img src="assets/img/appsus-logo.png" />
+                    </div>
 
                 </Link>
-    
-            <img onClick={this.toggleMenu} className="app-hamburger-icon" src="assets/img/app-hamburger-icon.png" alt="" />
-            <nav ref={this.inputRef} className="main-nav-list" >
+
+                <img onClick={this.toggleMenu} className="app-hamburger-icon" src="assets/img/app-hamburger-icon.png" alt="" />
+                <nav ref={this.inputRef} className="main-nav-list" >
                     <NavLink onClick={this.closeMenu} exact to="/">Home</NavLink>
                     <NavLink onClick={this.closeMenu} to="/note">Notes</NavLink>
                     <NavLink onClick={this.closeMenu} to="/book">Books</NavLink>
                     <NavLink onClick={this.closeMenu} to="/mail">Mail</NavLink>
                     <NavLink onClick={this.closeMenu} to="/about">About</NavLink>
                 </nav>
-        </div>
-        <UserMsg/>
-    </header>
+            </div>
+            <UserMsg />
+        </header>
     }
 }
 
